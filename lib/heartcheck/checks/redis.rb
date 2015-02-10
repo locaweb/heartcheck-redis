78472decf048c86cc1966cf39453e6fe6ba2cb8c
Marcio Trindade <marciotrindade@gmail.com>
2015-02-10 17:24:15 -0200

    refactoring redis check

--- lib/heartcheck/checks/redis.rb
+++ lib/heartcheck/checks/redis.rb
@@ -8,30 +8,52 @@ module Heartcheck
       # @retun [void]
       def validate
         services.each do |service|
-          con = service[:connection]
+          connection = service[:connection]
 
-          unless (con.set 'check_test', 'heartcheck') == 'OK'
-            append_error(service, :set)
-          end
-          unless (con.get 'check_test') == 'heartcheck'
-            append_error(service, :get)
-          end
-          append_error(service, :delete) unless (con.del 'check_test') == 1
+          append_error(service[:name], :set) unless set?(connection)
+          append_error(service[:name], :get) unless get?(connection)
+          append_error(service[:name], :delete) unless del?(connection)
         end
       end
 
       private
 
+      # test if can write on redis
+      #
+      # @param con [Redis] an instance of redis
+      #
+      # @return [Bollean]
+      def set?(con)
+        con.set('check_test', 'heartcheck') == 'OK'
+      end
+
+      # test if can read on redis
+      #
+      # @param con [Redis] an instance of redis
+      #
+      # @return [Bollean]
+      def get?(con)
+        con.get('check_test') == 'heartcheck'
+      end
+
+      # test if can delete on redis
+      #
+      # @param con [Redis] an instance of redis
+      #
+      # @return [Bollean]
+      def del?(con)
+        con.del('check_test') == 1
+      end
+
       # customize the error message
       # It's called in Heartcheck::Checks::Base#append_error
       #
-      # @param service [Hash]
-      # @option params [String] :name A identifier of service
+      # @param name [String] An identifier of service
       # @param key_error [Symbol] name of action
       #
       # @return [void]
-      def custom_error(service, key_error)
-        @errors << "#{service[:name]} fails to #{key_error}"
+      def custom_error(name, key_error)
+        @errors << "#{name} fails to #{key_error}"
       end
     end
   end
--- spec/heartcheck/checks/redis_spec.rb
+++ spec/heartcheck/checks/redis_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe Heartcheck::Checks::Redis do
   let(:connection) { ::Redis.new }
   let(:check_errors) { subject.instance_variable_get(:@errors) }
+
   subject do
     described_class.new.tap do |c|
       c.add_service(name: 'Default check', connection: connection)
@@ -8,7 +9,7 @@ RSpec.describe Heartcheck::Checks::Redis do
   end
 
   describe '#validate' do
-    context 'with success' do
+    context 'when nothing fails' do
       it 'calls set get and delete' do
         expect(connection).to receive(:set).and_call_original
         expect(connection).to receive(:get).and_call_original
@@ -18,35 +19,46 @@ RSpec.describe Heartcheck::Checks::Redis do
       end
     end
 
-    context 'with error' do
+    context 'when something fails' do
       before do
         allow(connection).to receive(:set).and_return('error')
         allow(connection).to receive(:get).and_return(nil)
         allow(connection).to receive(:del).and_return(0)
       end
 
-      it 'sets error for each action' do
-        error_messages = [
-          'Default check fails to set',
-          'Default check fails to get',
-          'Default check fails to delete'
-        ]
-        subject.validate
-        expect(check_errors).to match_array(error_messages)
+      context 'with default errors' do
+        before do
+          subject.validate
+        end
+
+        it { expect(check_errors).to include('Default check fails to set') }
+        it { expect(check_errors).to include('Default check fails to get') }
+        it { expect(check_errors).to include('Default check fails to delete') }
       end
 
-      it 'uses custom error message' do
-        error_messages = [
-          "Default check can't set a value",
-          "Default check can't get a value",
-          "Default check can't delete a value"
-        ]
-        subject.on_error do |errors, service, key_error|
-          errors << "#{service[:name]} can't #{key_error} a value"
+      context 'with custom errors' do
+        before do
+          subject.on_error do |errors, name, key_error|
+            errors << "#{name} can't #{key_error} a value"
+          end
+          subject.validate
         end
-        subject.validate
 
-        expect(check_errors).to match_array(error_messages)
+        context 'get' do
+          let(:msg) { 'Default check can\'t set a value' }
+          it { expect(check_errors).to include(msg) }
+        end
+
+        context 'get' do
+          let(:msg) { 'Default check can\'t get a value' }
+          it { expect(check_errors).to include(msg) }
+        end
+
+        context 'get' do
+          let(:msg) { 'Default check can\'t delete a value' }
+          it { expect(check_errors).to include(msg) }
+        end
+
       end
     end
   end
